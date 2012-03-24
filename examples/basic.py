7f30de6fe20d6a654a3419a8fe921a34d514ac18
Javier López <lopez.pardo@gmail.com>
2012-03-24 20:24:13 +0100

    Examples folder

---
+++ examples/basic.py
@@ -0,0 +1,10 @@
+from pycheck import check
+
+def basic(param1, param2=None):
+    check(param1).exists()
+    check(param2).dont.exists()
+
+
+if __name__ == "__main__":
+    basic(3)
+    basic(4, 5)
