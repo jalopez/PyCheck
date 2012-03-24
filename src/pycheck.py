12411b74eb39f318eba01c9beeffb64bc4ccb0c1
Javier López <lopez.pardo@gmail.com>
2012-03-24 18:39:54 +0100

    Basic tests

--- src/pycheck.py
+++ src/pycheck.py
@@ -1,2 +1,29 @@
-def check(variable):
-	pass
+def check(value):
+	return Variable(value)
+
+
+
+class Variable:
+	def __init__(self, value):
+		self.value = value
+
+	def exists(self):
+		if self.value is None:
+			raise CheckError('%s does not exist' % (self.value))
+		else:
+			return self
+
+	def is_None(self):
+		try: 
+			self.exists()
+		except CheckError:
+			return self
+		else:
+			raise CheckError('%s has value' % (self.value))
+
+
+class CheckError(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
--- test/test_pycheck.py
+++ test/test_pycheck.py
@@ -1,9 +1,28 @@
 import unittest
 
-from pycheck import check
+from pycheck import check, CheckError
 
+
+class TestBasic(unittest.TestCase):
+	def test_exists(self):
+		check(1).exists()
+
+		checker = check(None)
+		self.assertRaises(CheckError, checker.exists)
+
+	def test_is_none(self):
+		check(None).is_None()
+
+		checker = check(3)
+		self.assertRaises(CheckError, checker.is_None)
+
+	def test_chain(self):
+		check(1).exists().exists()
 
 class TestNumber(unittest.TestCase):
 
 	def test_accept_number(self):
 		check(3)
+
+	#def test_is_number(self):
+	#	check(0).is_number()
